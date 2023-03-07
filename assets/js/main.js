f54e24c0a04e3c1f0e3b845ec2061f8357954428
Fra <eso.fra24@gmail.com>
2023-03-07 15:17:37 +0100

    add flowchart in main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/* // Decrizione passaggi:
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
+
+//1. declare var: in this case are gonna be used const and let variables
+//2. write text in window prompt: use function prompt()
+//3. tieign var: concatenation constant var
+//4. displaing output
