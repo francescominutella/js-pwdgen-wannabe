b08b1b3638c73ff6333fe802534ddf9be6fef715
Fra <eso.fra24@gmail.com>
2023-03-07 15:23:14 +0100

    add var in js and print in console _ commented link in head

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,14 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
 
 //1. declare var: in this case are gonna be used const and let variables
+
+const firstName = prompt("Please enter user name:");
+const surName = prompt("Please enter user surname:");
+let favoriteColor = prompt("Please enter user's favorite color:");
+
+console.log(firstName, surName, favoriteColor)
+
 //2. write text in window prompt: use function prompt()
 //3. tieign var: concatenation constant var
 //4. displaing output
+
